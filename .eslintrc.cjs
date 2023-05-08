51cf9afda7fc10b29075925f854f846f11d933ed
Cristina <cristinaforescampos1992@gmail.com>
2023-05-08 13:10:35 +0200

    Add workflows and husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,10 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: ["recommended", "prettier"],
-  overrides: [
-    {
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-      ],
-    },
-  ],
-
+  env: { browser: true, es2021: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
-  rules: {},
+  rules: {
+    "no-undef": "off",
+  },
 };
