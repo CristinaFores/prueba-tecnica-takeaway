9878a4288de432727da608bd6a1a6c8a2846de42
Cristina <cristinaforescampos1992@gmail.com>
2023-05-09 16:07:34 +0200

    Function handleRestore in  AllRegions

--- src/components/AllRegions/AllRegions.tsx
+++ src/components/AllRegions/AllRegions.tsx
@@ -8,11 +8,22 @@ const AllRegions = () => {
 
   useLoadData();
 
+  const handleRestore = () => {
+    window.localStorage.removeItem("my-storage");
+    window.location.reload();
+  };
   return (
     <>
-      <div className="bg-red flex flex-col items-center">
-        <h2 className="text-2xl px-2 py-2 mb-1">Documento</h2>
+      <div className="bg-red flex flex-col items-center gap-4">
+        <h2 className="text-2xl mt-2">Documento</h2>
         <span className="w-screen h-0.5 bg-gradient-to-r from-slate-300 to-primary-light"></span>
+
+        <button
+          className="bg-primary border-3 border-solid text-primary-light p-2 px-4 rounded-lg font-semibold focus:ring-2 focus:ring-black focus:outline-none hover:bg-primary-light hover:text-primary cursor-pointer hover:border-primary hover:border-3 hover:border-solid focus:hidden"
+          onClick={handleRestore}
+        >
+          Restaurar
+        </button>
       </div>
       <Regions imageUrl={imageUrl} pointsArray={totalRegions} />
     </>
